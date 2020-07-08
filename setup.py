f6163435e85f0340e8344812ab29341e05cf29e3
Orkut Karacalik <orkutkaracalik@hotmail.com>
2020-07-08 09:07:19 +0200

    :recycle: refactoring

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='web_tracking',
       version='0.1',
@@ -6,12 +6,13 @@ setup(name='web_tracking',
       url='https://github.com/gesiscss/web_tracking',
       author='Orkut Karacalik',
       author_email='okaracalik12@gmail.com',
+      python_requires='>=3.6',
       license='gpl',
-      packages=['web_tracking'],
+      packages=find_packages(include=["web_tracking", "web_tracking.*"]),
       install_requires=[
-          'pandas',
-          'matplotlib',
-          'statsmodels',
-          'scipy'
+          'pandas==1.0.5',
+          'matplotlib==3.2.2',
+          'statsmodels==0.11.1',
+          'scipy==1.5.0'
       ],
       zip_safe=False)
